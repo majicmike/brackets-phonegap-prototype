b258513b09355bb159cbbfa3c7b9b3b99e0956b1
Christian Cantrell <christian.cantrell@gmail.com>
2012-08-16 15:51:33 -0400

    _Almost_ passes JSLint now.

--- main.js
+++ main.js
@@ -5,14 +5,14 @@
 define(function (require, exports, module) {
     'use strict';
 
-    var CommandManager = brackets.getModule("command/CommandManager"),
-        Menus          = brackets.getModule("command/Menus"),
-        Commands                = brackets.getModule("command/Commands"),
-        EditorManager           = brackets.getModule("editor/EditorManager"),
-        ProjectManager           = brackets.getModule("project/ProjectManager"),
-        NativeFileSystem           = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
-        FileUtils           = brackets.getModule("file/FileUtils"),
-        DocumentManager         = brackets.getModule("document/DocumentManager");
+    var CommandManager   = brackets.getModule("command/CommandManager"),
+        Menus            = brackets.getModule("command/Menus"),
+        Commands         = brackets.getModule("command/Commands"),
+        EditorManager    = brackets.getModule("editor/EditorManager"),
+        ProjectManager   = brackets.getModule("project/ProjectManager"),
+        NativeFileSystem = brackets.getModule("file/NativeFileSystem").NativeFileSystem,
+        FileUtils        = brackets.getModule("file/FileUtils"),
+        DocumentManager  = brackets.getModule("document/DocumentManager");
 
     // First, register a command - a UI-less object associating an id to a handler
     var local_require = require;
@@ -20,12 +20,9 @@ define(function (require, exports, module) {
 
     var PG_PROJECT_ASSOCIATION = "pg.toggleProjectAssociation";   // package-style naming to avoid collisions
 
-
     // Local modules
     require('phonegapbuild');
     var phonegapbuild = new PhoneGapBuild();
-
-
 
     function getAssociatedID() {
         var projectPath = ProjectManager.getProjectRoot().fullPath;
@@ -34,30 +31,23 @@ define(function (require, exports, module) {
     }
 
     function togglePGMenu(force) {
-        if (typeof (force) === 'undefined') {
+        if (typeof (force) === "undefined") {
             force = "";
         }
 
-
-        console.log("TogglePGMenu called");
         var $pgMenu = $('#pg-menu');
-
 
         if (force.length > 0) {
             if (force === "open") {
                 $pgMenu.css("display", "block");
-                console.log("Verdict open");
             } else if (force === "close") {
                 $pgMenu.css("display", "none");
-                console.log("Verdict close");
             }
         } else {
             if ($pgMenu.css("display") === "block") {
                 $pgMenu.css("display", "none");
-                console.log("Verdict close");
             } else {
                 $pgMenu.css("display", "block");
-                console.log("Verdict open");
             }
         }
     }
@@ -76,10 +66,8 @@ define(function (require, exports, module) {
         if (force.length > 0) {
             if (force === "open") {
                 $pgInterface.show();
-                console.log("Open!");
             } else if (force === "close") {
                 $pgInterface.hide();
-                console.log("Close!");
             }
         } else {
             if ($pgInterface.css("display") === "none") {
@@ -92,19 +80,9 @@ define(function (require, exports, module) {
         EditorManager.resizeEditor();
     }
 
-    function handlePGMenuLogin(e) {
-        e.preventDefault();
-        createPGLoginForm();
-        togglePGPanelDisplay("open", "250px");
-        togglePGMenu("close");
-    }
-
-
     function getPGList() {
         phonegapbuild.getList();
     }
-
-
 
 
     function errorHandler(error) {
@@ -143,12 +121,6 @@ define(function (require, exports, module) {
         $("#pg-menu-toggle img").attr("src", iconURL);
     }
 
-    function createLoginMenuItem() {
-        $("#pg-menu").prepend('<li id="login-holder"><a id="pg-login" href="">Login</li>');
-        $('#pg-login').click(handlePGMenuLogin);
-    }
-
-
     function handlePGMenuList(e) {
         e.preventDefault();
         togglePGMenu("close");
@@ -161,39 +133,11 @@ define(function (require, exports, module) {
         window.alert(list);
     }
 
-    function handlePBRebuildRequested(e) {
-        var id = getAssociatedID();
-        setMenuToBuilding();
-        phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
-        phonegapbuild.getProjectStatus(id);
-    }
-
-    function handlePGMenuLogout(e) {
-        e.preventDefault();
-        phonegapbuild.logout();
-        $("#pg-menu").empty();
-        createLoginMenuItem();
-        togglePGMenu("close");
-        setMenuToLogout();
-        removePGContextMenu();
-    }
-
-    function handlePGMenuRebuild(e) {
-        var id = getAssociatedID();
-        e.preventDefault();
-        togglePGMenu("close");
-        phonegapbuild.addListener("rebuildrequested", handlePBRebuildRequested);
-        phonegapbuild.rebuild(id);
-    }
-
     function updateIncompleteCount(count) {
         var $orig = $('#incomplete-count').text();
         var stringCount = count.toString();
 
-        console.log($orig + " vs " + count);
-
         if ($orig !== stringCount) {
-            console.log("Resetting stuff");
             if (count > 0) {
                 $('#incomplete-count').show();
             } else {
@@ -202,47 +146,6 @@ define(function (require, exports, module) {
 
             $('#incomplete-count').text(stringCount);
         }
-    }
-
-    function handlePGStatusResponse(e) {
-        var project = e.detail;
-        var propertyname;
-
-        var subtable = '<table class="condensed-table">';
-
-        for (propertyname in project.status) {
-            subtable += '<tr><th>' + propertyname + '</th><td>' + project.status[propertyname] + '</td></tr>';
-        }
-        subtable += '</table>';
-
-        $("#pg-project-title").text(project.title);
-        $("#pg-project-description").text(project.description);
-        $("#pg-project-status").html(subtable);
-
-        if (project.complete === true) {
-            setMenuToAssociated();
-            updateIncompleteCount(0);
-        } else {
-            setMenuToBuilding();
-            updateIncompleteCount(project.incompleteCount);
-        }
-
-    }
-
-    function handlePGMenuViewStatus(e) {
-        e.preventDefault();
-        var id = getAssociatedID();
-        console.log("Handling status call");
-        togglePGMenu("close");
-        createPGStatusView();
-
-        if ($("#pg-interface").css("display") === 'none') {
-            console.log("opening panel");
-            togglePGPanelDisplay("open", "360px");
-        }
-
-        phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
-        phonegapbuild.getProjectStatus(id);
     }
 
     function orderPGMenu() {
@@ -254,38 +157,6 @@ define(function (require, exports, module) {
         $logoutDivider.detach();
         $pgMenu.append($logoutDivider);
         $pgMenu.append($logout);
-
-    }
-
-    function createViewStatusMenuItem() {
-        $("#pg-menu").append('<li id="pg-status-holder"><a id="pg-status" href="">View Status</li>');
-        $("#pg-status").click(handlePGMenuViewStatus);
-        orderPGMenu();
-    }
-
-    function removeViewStatusMenuItem() {
-        $("#pg-status-holder").remove();
-    }
-
-    function createRebuildMenuItem() {
-        if ($("#pg-rebuild-holder").length === 0) {
-            $("#pg-menu").append('<li id="pg-rebuild-holder"><a id="pg-rebuild" href="">Rebuild</li>');
-            $("#pg-rebuild").click(handlePGMenuRebuild);
-            orderPGMenu();
-        }
-    }
-
-    function removeRebuildMenuItem() {
-        $("#pg-rebuild-holder").remove();
-    }
-
-    function createLogoutMenuItem() {
-        if ($("#pg-logout-holder").length === 0) {
-            $("#pg-menu").append('<li id="pg-logout-divider"><hr class="divider" /></li>');
-            $("#pg-menu").append('<li id="pg-logout-holder"><a id="pg-logout" href="">Logout</li>');
-            $("#pg-logout").click(handlePGMenuLogout);
-            orderPGMenu();
-        }
     }
 
     function createListMenuItem() {
@@ -300,10 +171,7 @@ define(function (require, exports, module) {
         var menu = Menus.getContextMenu("project-context-menu");
         menu.addMenuDivider();
         menu.addMenuItem(PG_PROJECT_ASSOCIATION);
-        console.log(menu);
     }
-
-
 
     function removePGContextMenu() {
         //REMOVE MENU NOT IMPLEMENTED YET
@@ -312,82 +180,32 @@ define(function (require, exports, module) {
         //console.log(menu);
     }
 
-    function checkAssociation() {
-
-        var id = getAssociatedID();
-
-
-        if ((typeof (id) === 'undefined') || (id === null)) {
-            CommandManager.get(PG_PROJECT_ASSOCIATION).setName("Link with PhoneGap Build");
-            removeRebuildMenuItem();
-            removeViewStatusMenuItem();
-            setMenuToIdle();
-            phonegapbuild.removeListener("statusresponse", handlePGStatusResponse);
-            updateIncompleteCount(0);
-            phonegapbuild.killTimers();
-        } else {
-            CommandManager.get(PG_PROJECT_ASSOCIATION).setName("Unlink with PhoneGap Build");
-            createRebuildMenuItem();
-            createViewStatusMenuItem();
-            phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
-            phonegapbuild.getProjectStatus(id);
-            setMenuToAssociated();
-
-        }
-
+    function handlePGLoginFailure() {
+        $('#pg-login-alert').show();
+        $('#pg-login-indicator').hide();
     }
 
-    function doAssociate(e) {
-        event.preventDefault();
-        var $id = $('#projectid').val();
-        var projectPath = ProjectManager.getProjectRoot().fullPath;
 
-        phonegapbuild.setAssociation(projectPath, $id);
-        togglePGPanelDisplay("close");
-        checkAssociation();
+    
+    function handlePGMenuLogout(e) {
+        e.preventDefault();
+        phonegapbuild.logout();
+        $("#pg-menu").empty();
+        createLoginMenuItem();
+        togglePGMenu("close");
+        setMenuToLogout();
+        removePGContextMenu();
     }
-
-    function createPGAssociation() {
-
-        var options = "";
-        var form = "";
-        var i = 0;
-
-        var projectList = phonegapbuild.list;
-
-        for (i = 0; i < projectList.length; i++) {
-            options += '<option value=' + projectList[i].id + '>' + projectList[i].title + '</option>';
-        }
-
-
-        form = '<form>' +
-            '   <label for="projectid">Project:</label>' +
-            '   <select id="projectid">' +
-            options +
-            '   </select><br />' +
-            '   <input id="submit-associate" type="submit" class="btn" name="sumbit" value="Link" /><br />' +
-            '</form>';
-        $('#pg-interface-content').empty();
-        $('#pg-interface-content').append(form);
-        $('#submit-associate').click(function () {doAssociate(); });
-
-    }
-
-    function handlePGAssociate() {
-        console.log("So association logic here");
-        var projectPath = ProjectManager.getProjectRoot().fullPath;
-        var id = phonegapbuild.getAssociation(projectPath);
-
-        if ((typeof (id) === 'undefined') || (id === null)) {
-            createPGAssociation();
-            togglePGPanelDisplay("open");
-        } else {
-            phonegapbuild.removeAssociation(projectPath);
-            checkAssociation();
+    
+    function createLogoutMenuItem() {
+        if ($("#pg-logout-holder").length === 0) {
+            $("#pg-menu").append('<li id="pg-logout-divider"><hr class="divider" /></li>');
+            $("#pg-menu").append('<li id="pg-logout-holder"><a id="pg-logout" href="">Logout</li>');
+            $("#pg-logout").click(handlePGMenuLogout);
+            orderPGMenu();
         }
     }
-
-
+    
     function handlePGLoginSuccess() {
         togglePGPanelDisplay("close");
         $("#login-holder").remove();
@@ -396,16 +214,8 @@ define(function (require, exports, module) {
         setMenuToIdle();
         getPGList();
         createPGContextMenu();
-
-
     }
-
-    function handlePGLoginFailure() {
-        $('#pg-login-alert').show();
-        $('#pg-login-indicator').hide();
-    }
-
-
+    
     function doLogin() {
         event.preventDefault();
         $('#pg-login-indicator').show();
@@ -417,11 +227,8 @@ define(function (require, exports, module) {
         phonegapbuild.login($username, $password);
     }
 
-
     function createPGLoginForm() {
-
         var iconURL = local_require.nameToUrl('../../../styles/images/throbber.gif').split('[')[0];
-
         var form = '<form id="pg-login-form">' +
                     '<div id="pg-login-alert" class="alert-message error">Check your username and password.</div>' +
                     '    <fieldset>' +
@@ -445,7 +252,18 @@ define(function (require, exports, module) {
         $('#pg-submit-login').css("margin-left", "-20px");
         $('#pg-login-alert').hide();
         $('#pg-login-indicator').hide();
+    }
 
+    function handlePGMenuLogin(e) {
+        e.preventDefault();
+        createPGLoginForm();
+        togglePGPanelDisplay("open", "250px");
+        togglePGMenu("close");
+    }
+
+    function createLoginMenuItem() {
+        $("#pg-menu").prepend('<li id="login-holder"><a id="pg-login" href="">Login</li>');
+        $('#pg-login').click(handlePGMenuLogin);
     }
 
     function createPGStatusView() {
@@ -456,10 +274,7 @@ define(function (require, exports, module) {
             '</table>';
         $('#pg-interface-content').empty();
         $('#pg-interface-content').append(table);
-
     }
-
-
 
     function handlePGMenu(e) {
         e.preventDefault();
@@ -519,25 +334,168 @@ define(function (require, exports, module) {
         $('#incomplete-count').css("margin-left", "-5px");
         $('#incomplete-count').hide();
 
-
-
         var $pgMenu = $('#pg-menu');
         $pgMenu.css("top", "10px");
         $pgMenu.css("right", "10px");
         $pgMenu.css("border-top", "1px solid #CCC");
         createLoginMenuItem();
+    }
 
+    function handlePGStatusResponse(e) {
+        var project = e.detail;
+        var propertyname;
+
+        var subtable = '<table class="condensed-table">';
+
+        for (propertyname in project.status) {
+            if (project.status.hasOwnProperty(propertyname)) {
+                subtable += '<tr><th>' + propertyname + '</th><td>' + project.status[propertyname] + '</td></tr>';
+            }
+        }
+        subtable += '</table>';
+
+        $("#pg-project-title").text(project.title);
+        $("#pg-project-description").text(project.description);
+        $("#pg-project-status").html(subtable);
+
+        if (project.complete === true) {
+            setMenuToAssociated();
+            updateIncompleteCount(0);
+        } else {
+            setMenuToBuilding();
+            updateIncompleteCount(project.incompleteCount);
+        }
+    }
+    
+    function handlePBRebuildRequested(e) {
+        console.log("handlePBRebuildRequested");
+        var id = getAssociatedID();
+        setMenuToBuilding();
+        phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
+        phonegapbuild.getProjectStatus(id);
+    }
+    
+    function handlePGMenuRebuild(e) {
+        var id = getAssociatedID();
+        e.preventDefault();
+        togglePGMenu("close");
+        phonegapbuild.addListener("rebuildrequested", handlePBRebuildRequested);
+        phonegapbuild.rebuild(id);
+    }
+
+    function handlePGMenuViewStatus(e) {
+        e.preventDefault();
+        var id = getAssociatedID();
+        togglePGMenu("close");
+        createPGStatusView();
+
+        if ($("#pg-interface").css("display") === 'none') {
+            togglePGPanelDisplay("open", "360px");
+        }
+        console.log("handlePGMenuViewStatus");
+
+        phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
+        phonegapbuild.getProjectStatus(id);
+    }
+
+    function createViewStatusMenuItem() {
+        $("#pg-menu").append('<li id="pg-status-holder"><a id="pg-status" href="">View Status</li>');
+        $("#pg-status").click(handlePGMenuViewStatus);
+        orderPGMenu();
+    }
+
+    function removeViewStatusMenuItem() {
+        $("#pg-status-holder").remove();
+    }
+
+    function createRebuildMenuItem() {
+        if ($("#pg-rebuild-holder").length === 0) {
+            $("#pg-menu").append('<li id="pg-rebuild-holder"><a id="pg-rebuild" href="">Rebuild</li>');
+            $("#pg-rebuild").click(handlePGMenuRebuild);
+            orderPGMenu();
+        }
+    }
+
+    function removeRebuildMenuItem() {
+        $("#pg-rebuild-holder").remove();
+    }
+
+    function checkAssociation() {
+        var id = getAssociatedID();
+        if ((typeof (id) === 'undefined') || (id === null)) {
+            console.log("handlePGStatusResponse 1");
+            CommandManager.get(PG_PROJECT_ASSOCIATION).setName("Link with PhoneGap Build");
+            removeRebuildMenuItem();
+            removeViewStatusMenuItem();
+            setMenuToIdle();
+            phonegapbuild.removeListener("statusresponse", handlePGStatusResponse);
+            updateIncompleteCount(0);
+            phonegapbuild.killTimers();
+        } else {
+            console.log("handlePGStatusResponse 2");
+            CommandManager.get(PG_PROJECT_ASSOCIATION).setName("Unlink with PhoneGap Build");
+            createRebuildMenuItem();
+            createViewStatusMenuItem();
+            phonegapbuild.addListener("statusresponse", handlePGStatusResponse);
+            phonegapbuild.getProjectStatus(id);
+            setMenuToAssociated();
+        }
     }
 
     function handlePGInitialize(e) {
-
         if (e.detail.tokenDefined === true) {
             handlePGLoginSuccess();
             checkAssociation();
-            console.log("Token was in localstorage");
         } else {
             console.log("Token was NOT in localstorage");
+        }
+    }
 
+
+    function doAssociate(e) {
+        event.preventDefault();
+        var $id = $('#projectid').val();
+        var projectPath = ProjectManager.getProjectRoot().fullPath;
+
+        phonegapbuild.setAssociation(projectPath, $id);
+        togglePGPanelDisplay("close");
+        checkAssociation();
+    }
+
+    function createPGAssociation() {
+        var options = "";
+        var form = "";
+        var i = 0;
+
+        var projectList = phonegapbuild.list;
+
+        for (i = 0; i < projectList.length; i++) {
+            options += '<option value=' + projectList[i].id + '>' + projectList[i].title + '</option>';
+        }
+
+
+        form = '<form>' +
+            '   <label for="projectid">Project:</label>' +
+            '   <select id="projectid">' +
+            options +
+            '   </select><br />' +
+            '   <input id="submit-associate" type="submit" class="btn" name="sumbit" value="Link" /><br />' +
+            '</form>';
+        $('#pg-interface-content').empty();
+        $('#pg-interface-content').append(form);
+        $('#submit-associate').click(function () {doAssociate(); });
+    }
+
+    function handlePGAssociate() {
+        var projectPath = ProjectManager.getProjectRoot().fullPath;
+        var id = phonegapbuild.getAssociation(projectPath);
+
+        if ((typeof (id) === 'undefined') || (id === null)) {
+            createPGAssociation();
+            togglePGPanelDisplay("open");
+        } else {
+            phonegapbuild.removeAssociation(projectPath);
+            checkAssociation();
         }
     }
 
@@ -547,13 +505,10 @@ define(function (require, exports, module) {
     phonegapbuild.addListener("initialized", handlePGInitialize);
     phonegapbuild.initialize();
 
-
     // close all dropdowns on ESC
     $(window.document).on("keydown", function (e) {
         if (e.keyCode === 27) {
             togglePGMenu("close");
         }
     });
-
-
 });
